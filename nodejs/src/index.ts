0f060ba0fa011833ceb1ccb7b23fffcd612f9c30
MiyabiBaba0827 <babamiyabi@babamiyabinoMacBook-Air.local>
2026-06-08 00:46:46 +0900

    add user api

--- nodejs/src/index.ts
+++ nodejs/src/index.ts
@@ -5,11 +5,11 @@ const PORT: number = process.env.PORT ? parseInt(process.env.PORT) : 3000;
 
 app.use(express.json());
 
-app.get('/', (req: Request, res: Response) => {
+app.get('/', (_req: Request, res: Response) => {
   res.json({ message: 'Welcome' });
 });
 
-app.get('/health', (req: Request, res: Response) => {
+app.get('/health', (_req: Request, res: Response) => {
   res.json({ status: 'OK', timestamp: new Date().toISOString() });
 });
 
